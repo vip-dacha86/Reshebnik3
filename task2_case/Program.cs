aa55806eb42bc09b1b923cd8eb44d171c2d4a712
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-18 14:21:55 +0500

    task2_case

---
+++ task2_case/Program.cs
@@ -0,0 +1,37 @@
+﻿//Напишите программу которая по заданному номеру четверти показывает диапазон
+//возможных координат точек в этой четверти (x и y)
+
+Console.Write("Введите номер четверти ");
+int quater = int.Parse(Console.ReadLine());
+
+switch (quater)
+ { 
+    case1:
+{
+    Console.WriteLine("x>0 y>0");
+    break;
+}
+case2:
+{
+    Console.WriteLine("x<0 y>0");
+    break;
+}
+case3:
+{
+    Console.WriteLine("x<0 y<0");
+    break;
+}
+case4:
+{
+    Console.WriteLine("x>0 y<0");
+    break;
+}
+default:{
+    Console.WriteLine("Введена неправильная четверть"());
+    break;
+}
+}
+
+
+
+
