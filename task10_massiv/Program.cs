accaac52acc34495956493af6b5b9230bdd03aec
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-20 21:14:38 +0500

    task10_massiv

---
+++ task10_massiv/Program.cs
@@ -0,0 +1,30 @@
+﻿int []arr = {1,5,4,3,2,6,7,1,1};
+void PrintArray(int[]array)
+{
+    int count = array.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($" {array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+ void selectionSort(int[] array)
+  {
+    for (int i = 0; i < array.Length-1;i++)
+    {
+        int minPosition = i ;
+    for (int j = i + 1; j < array.Length; j++)
+     {
+     if (array [j]< array[minPosition])minPosition =j;
+     }
+     int temporary = array[i];
+     array[i] = array[minPosition];
+     array[minPosition] = temporary;
+    }
+  }
+     PrintArray(arr);
+     selectionSort(arr);
+
+     PrintArray(arr);
