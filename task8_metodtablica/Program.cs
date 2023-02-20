0c25c1b23748aa6f7a0909e5c3903a7c9c9def0b
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-20 15:37:43 +0500

    task8_metodtablica

--- task8_metodtablica/Program.cs
+++ task8_metodtablica/Program.cs
@@ -14,7 +14,7 @@ for (int i =2; i <= 10; i ++)
 {
       for (int j = 2; j <= 10; i ++)
     {
-        Console.WriteLine($"{i}*{j}={i*j}");
+        Console.WriteLine($"{i} * {j}={i * j}");
     }
-    Console.WriteLine();
 }
+Console.WriteLine();
