062fd4155587a9f8ddecdb9729c1463d5adc1c18
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-20 11:49:46 +0500

    task5_metod 1

---
+++ task5_metod1/Program.cs
@@ -0,0 +1,15 @@
+﻿void Method2(string msg)
+{
+    Console.WriteLine(msg);
+}
+// Method2(msg:"Текст сообщения");
+void Method21(string msg,int count)
+{
+    int i = 0;
+    while (i < count)
+    {
+        Console.WriteLine(msg);
+        i++;
+    }
+}
+Method21("Софа", 8 );
