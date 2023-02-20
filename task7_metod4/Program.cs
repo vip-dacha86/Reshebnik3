2b90c275abeea87a68e56be28a005f85dca2458f
vip-dacha86 <vip-dacha86@yandex.ru>
2023-02-20 12:27:57 +0500

    task7_metod4

---
+++ task7_metod4/Program.cs
@@ -0,0 +1,14 @@
+﻿string Method4(int count,string text)
+{
+    int i = 0;
+    string result = string.Empty;
+
+    while(i < count)
+{
+    result = result +text;
+    i++;
+}
+return result;
+}
+string res = Method4(10," sofa,anka");
+Console.WriteLine(res);
